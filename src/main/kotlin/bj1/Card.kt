88c80d391188af309b3219da8fae275f4132bdae
MrinalRanpise <ranpise.mrinal@gmail.com>
2023-04-26 21:04:51 +0530

    Use of init keyword for setting some validations for constuctor

--- src/main/kotlin/bj1/Card.kt
+++ src/main/kotlin/bj1/Card.kt
@@ -4,11 +4,24 @@ import java.lang.IllegalArgumentException
 
 class Card(val value:Int , val suit:Int){
 
+    init {          //kind of like constructor
+        require(value in 1..13)  //Used to check illegal argument exception,this returns true or false , If this return false then it throws illegalArgumentException
+        require(suit in 1..4)
+    }
+
 //    val suitName:String get() {
 //        return "Spades"
 //    }
-    val valueName:String get() ="ABC"  //Another way of writing get method
+   // val suitName:String get() ="Spades"  //Another way of writing get method
 
+    val valueName :String get() = when(value){
+        1 -> "ace"
+        in 2..10 -> value.toString()
+        11 -> "Jack"
+        12 -> "Queen"
+        13 -> "King"
+        else -> throw IllegalArgumentException()
+    }
 
     val suitName:String get() = when(suit) {
         1 -> "Spades"
@@ -26,4 +39,8 @@ class Card(val value:Int , val suit:Int){
 //            else -> throw IllegalArgumentException()
 //        }
 //    }
+
+
+
+
 }
--- src/test/kotlin/bj1/CardTest.kt
+++ src/test/kotlin/bj1/CardTest.kt
@@ -1,7 +1,9 @@
 package bj1
 
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
 import kotlin.test.assertEquals
+import kotlin.test.fail
 
 class CardTest {
 
@@ -14,9 +16,25 @@ class CardTest {
         assertEquals(1, c1.value)
         assertEquals(1, c1.suit)
         assertEquals("Spades", c1.suitName)
+        assertEquals("ace", c1.valueName)
         assertEquals(13, c2.value)
         assertEquals(4, c2.suit)
         assertEquals("Clubs", c2.suitName)
+        assertEquals("King", c2.valueName)
+
+            //testcase for manual throwing IllegalArgumentException
+        try {
+             Card(1,5)
+            fail("Code not reachable") //This code is unreachable , so we use fail to check
+        } catch (e: IllegalArgumentException) {
+
+        }
+        try {
+             Card(14,1)
+            fail("Code not reachable") //This code is unreachable , so we use fail
+        } catch (e: IllegalArgumentException) {
+
+        }
     }
 
     @Test
@@ -31,6 +49,15 @@ class CardTest {
             else -> "Clubs"
         }
         println("y= $y")
+    }
+
+    @Test
+    fun t3(){
+        val x =2..6  // here x is of type IntRange
+        println(x.contains(7))
+         // OR
+        println(6 in x)  //we can use in operator instead of contains
+
     }
 
 }
