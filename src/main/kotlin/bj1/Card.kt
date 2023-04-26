7ef33df417d02d2890c7d9ad41a8881b6edb5a8c
MrinalRanpise <ranpise.mrinal@gmail.com>
2023-04-26 18:14:11 +0530

    Used When and getter method

--- src/main/kotlin/bj1/Card.kt
+++ src/main/kotlin/bj1/Card.kt
@@ -1,3 +1,29 @@
 package bj1
 
-class Card(val value:Int , val suit:Int)
+import java.lang.IllegalArgumentException
+
+class Card(val value:Int , val suit:Int){
+
+//    val suitName:String get() {
+//        return "Spades"
+//    }
+    val valueName:String get() ="ABC"  //Another way of writing get method
+
+
+    val suitName:String get() = when(suit) {
+        1 -> "Spades"
+        2 -> "Hearts"
+        3 -> "Diamonds"
+        4 -> "Clubs"
+        else -> throw IllegalArgumentException()
+    }
+//    val suitName:String get() {   // can also be written like this
+//        when(suit) {
+//            1 ->return "Spades"
+//            2 ->return "Hearts"
+//            3 ->return "Diamonds"
+//            4 -> return "Clubs"
+//            else -> throw IllegalArgumentException()
+//        }
+//    }
+}
--- src/test/kotlin/bj1/CardTest.kt
+++ src/test/kotlin/bj1/CardTest.kt
@@ -6,15 +6,31 @@ import kotlin.test.assertEquals
 class CardTest {
 
     @Test
-    fun t1(){
+    fun t1() {
 
-        val c1 =Card(1,1)
-        val c2 =Card(13,4)
+        val c1 = Card(1, 1)
+        val c2 = Card(value = 13, suit = 4)
 
-       assertEquals(1,c1.value)
-       assertEquals(1,c1.suit)
-       assertEquals(13,c2.value)
-       assertEquals(4,c2.suit)
-
+        assertEquals(1, c1.value)
+        assertEquals(1, c1.suit)
+        assertEquals("Spades", c1.suitName)
+        assertEquals(13, c2.value)
+        assertEquals(4, c2.suit)
+        assertEquals("Clubs", c2.suitName)
     }
+
+    @Test
+    fun t2() {
+        val x = if (4 > 3) "afd" else "ersa"   // if statement is expression
+        println(x)
+        val suit = 2
+        val y = when (suit) {  //act as a switch
+            1 -> "Spades"
+            2 -> "Hearts"
+            3 -> "Diamond"
+            else -> "Clubs"
+        }
+        println("y= $y")
+    }
+
 }
